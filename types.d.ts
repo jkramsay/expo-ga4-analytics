48156ad8d1d8d92be1b6d71a5a4271befd779a7f
James Ramsay <jamesramsay@Jamess-MacBook-Air.local>
2023-03-08 09:48:31 -0500

    Update type for eventparams

--- types.d.ts
+++ types.d.ts
@@ -1,9 +1,14 @@
+export interface EventParameter {
+  [key: string]: any;
+}
+
 export class Analytics {
   constructor(measurementId: string, apiSecret: string, options?: Options);
-  track(eventName: string, params: {[string:any]: any}): Promise<void>;
+  track(eventName: string, params: EventParameter): Promise<void>;
   setUserId(userId: string): void;
 }
 
 export interface Options {
   debug?: boolean;
 }
+
